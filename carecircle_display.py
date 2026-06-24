588e970eb4a317537bbca9c17f1deee15995b2b7
CmdrCloud <a.garoufalias@gmail.com>
2026-06-23 21:00:04 -0400

    adding tzlocal fix

--- carecircle_display.py
+++ carecircle_display.py
@@ -14,6 +14,7 @@ import socket
 from datetime import datetime
 
 sys.path.insert(0, '/home/pi/Inkycal/inkycal/display/drivers')
+sys.path.insert(0, '/home/pi/Inkycal/venv/lib/python3.7/site-packages')
 
 logging.basicConfig(
     level=logging.INFO,
